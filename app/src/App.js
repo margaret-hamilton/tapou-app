e2ddeb1feb509b15637505c9fb5e9fdb523a8f48
Vitor Ferreira Bem Silva <vitorfbs.ti@gmail.com>
2022-05-25 22:14:37 -0300

    add menu

--- app/src/App.js
+++ app/src/App.js
@@ -3,42 +3,56 @@ import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Done from "./pages/Done";
 import Hazards from "./pages/Hazards";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+  Container,
+  Row,
+  Col,
+  Card,
+  Button,
+  Form,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-          </ul>
-        </nav>
+    <Container>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Tapou</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Início</Nav.Link>
+              <Nav.Link href="/hazards">Conformidades</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-        {/* A <Routes> looks through its children <Route>s and
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Routes> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Routes>
-          <Route path="/register" element={<Register />}>
-            
-          </Route>
-          <Route path="/done" element={<Done />}>
-            
-          </Route>
-          <Route path="/hazards" element={<Hazards />}>
-            
-          </Route>
-          <Route path="/" element={<Home />}>
-            
-          </Route>
-        </Routes>
-      </div>
-    </Router>
+          <Routes>
+            <Route path="/register" element={<Register />}></Route>
+            <Route path="/done" element={<Done />}></Route>
+            <Route path="/hazards" element={<Hazards />}></Route>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </Container>
   );
 }
